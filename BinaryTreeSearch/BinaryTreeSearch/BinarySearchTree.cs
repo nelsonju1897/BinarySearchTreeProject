d98acc8381da35594070229b9e41a085acc9ec77
nelsonju1897 <nelsonju1897@gmail.com>
2019-11-11 15:17:42 -0600

    addNode method completed. Working on search method.

---
+++ BinaryTreeSearch/BinaryTreeSearch/BinarySearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeSearch
+{
+    class BinarySearchTree
+    {
+        public Node root;
+        
+
+        public void addNode(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.item = data;
+            if(root == null)
+            {
+                root = newNode;
+                return;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if(data <= current.item)
+                    {
+                        current = current.left;
+                        if(current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if(current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+            
+
+        }
+
+        public void searchNode(int data)
+        {
+
+        }
+
+    }
+}
--- BinaryTreeSearch/BinaryTreeSearch/Program.cs
+++ BinaryTreeSearch/BinaryTreeSearch/Program.cs
@@ -10,6 +10,21 @@ namespace BinaryTreeSearch
     {
         static void Main(string[] args)
         {
+
+            //create new list
+            //add data to list - is there a root? Yes - add to next part of tree. No - set root
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.addNode(10);
+            bst.addNode(5);
+            bst.addNode(6);
+            bst.addNode(11);
+            bst.addNode(4);
+            bst.addNode(2);
+            bst.addNode(15);
+            bst.addNode(100);
+            bst.addNode(5);
+            bst.addNode(10);
+            Console.ReadLine();
         }
     }
 }
